99166cfdcf014dce5354245c302f1e8302175490
Kev2Cod <kevinity310@gmail.com>
2023-01-29 13:48:54 +0700

    add wait_for_db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,37 +1,28 @@
-""" 
+"""
 Test custom Django management command.
 """
-
 from unittest.mock import patch
-
 from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """ Test commands. """
-    
+    """Test commands."""
+
     def test_wait_database_db_ready(self, pathced_check):
-        """ Test Waiting For Databases if Database Ready """
-        pathced_check.return_value = True 
-        
+        """Test Waiting For Databases if Database Ready"""
+        pathced_check.return_value = True
         call_command("wait_for_db")
-        
-        pathced_check.assert_called_once_with(database=['default'])
-    
+        pathced_check.assert_called_once_with(databases=['default'])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, pathced_check):
-        """ Test waiting for database when getting Operation Error """
-        pathced_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]
-    
+        """ Test waiting for database when getting Operation Error """  # noqa: E501
+        pathced_check.side_effect = [
+            Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
         call_command("wait_for_db")
-        
         self.assertEqual(pathced_check.call_count, 6)
-        
-        pathced_check.assert_called_with(database=['default'])
-        
-        
+        pathced_check.assert_called_with(databases=['default'])
